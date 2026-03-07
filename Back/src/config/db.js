3b96afb3cf94c2b2e95eeb767d366ff6544134da
Dani <danilago1998@gmail.com>
2026-03-07 16:05:33 +0000

    Añadido puerto DB para Railway

--- Back/src/config/db.js
+++ Back/src/config/db.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,   // necesario indicar el puerto para la conexion con Railway
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
