8534039fbb7b904cb1826ddc7adac6d5c7cf8126
Dani <danilago1998@gmail.com>
2026-03-07 15:37:45 +0000

    Configuración backend para deploy en Render

--- Back/src/server.js
+++ Back/src/server.js
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import app from "./app.js";
 
+const PORT = process.env.PORT || 3001;
+
 console.log("DB_NAME =", process.env.DB_NAME);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Backend activo en puerto ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Backend activo en puerto ${PORT}`);
 });
