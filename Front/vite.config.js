78d1bc7b64f2f0a407fa737f7e03c456fa49cead
broom32 <laojru@gmail.com>
2026-05-15 22:38:48 +0100

    fix local

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://myparking-backend.onrender.com',
+        target: 'http://localhost:3006',
         changeOrigin: true,
         secure: true,
       }
